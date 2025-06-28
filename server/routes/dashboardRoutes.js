56219ef67f06fa2cdbd1c5dd30b13be1b955683c
Vijay Misal <misalvijay153@gmail.com>
2025-06-28 12:46:40 +0530

    separated server from client for deployment

--- server/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboardData } from '../controllers/dashboardController.js';
-import { authenticateToken } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', authenticateToken, getDashboardData);
-
-export default router;
